4921393d5af51487230632256db9cc093894342b
quangtri303 <quangtri3032001@gmail.com>
2026-01-02 00:07:29 +0700

    init

---
+++ src/test/java/com/tqt/shawttee/ShawtteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqt.shawttee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShawtteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
